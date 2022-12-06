95ad22459adebabd4059a1a9bce4cb01ef0f0cd3
alexgrek <alexgrek.hq@gmail.com>
2022-12-07 00:45:42 +0200

    push

--- replacer.sh
+++ replacer.sh
@@ -1,4 +1,4 @@
 cd $1
 echo "Replacing localhost to ${WS_HOSTNAME} in files:"
-grep -rl "localhost:3030" $1
-grep -rl "localhost:3030" $1 | xargs sed -i "s/localhost:3330/${WS_HOSTNAME}/g"
+grep -rl "localhost:3330" $1
+grep -rl "localhost:3330" $1 | xargs sed -i "s/localhost:3330/${WS_HOSTNAME}/g"
